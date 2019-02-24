8f0740a4375245344575fbb95e1272a5e2845beb
/Z3X_ID <46043473+Z3X-Gans@users.noreply.github.com>
2019-02-24 16:57:26 +0700

    Add files via upload

---
+++ Calculatorv2.py
@@ -0,0 +1,43 @@
+''' Program kalkulator sederhana untuk menjumlah, mengurang, mengali, dan membagi bilangan dengan menggunakan fungsi ''' 
+
+# fungsi penjumlahan 
+def add(x, y): 
+return x + y 
+
+# fungsi pengurangan 
+def subtract(x, y): 
+return x - y 
+
+# fungsi perkalian 
+def multiply(x, y): 
+return x * y 
+
+# fungsi pembagian 
+def divide(x, y): 
+return x / y 
+
+# menu operasi 
+print("Author : ./Z3X_ID")
+print("Team : BOJONEGORO CYBER SECURITY")
+print("Blog : https://bojonegoroprogrammer.zone.id")
+print("Notify : No Recode Ea,Buat Sendiri")
+print("Indonesian Programmer")
+print("Pilih Operasi.") print("1.Jumlah") 
+print("2.Kurang") 
+print("3.Kali") 
+print("4.Bagi") 
+
+# Meminta input dari user choice = input("Masukkan pilihan(1/2/3/4): ")
+
+num1 = int(input("Masukkan bilangan pertama: ")) 
+num2 = int(input("Masukkan bilangan kedua: ")) 
+
+if choice == '1': print(num1,"+",num2,"=", add(num1,num2)) 
+
+elif choice == '2': print(num1,"-",num2,"=", subtract(num1,num2)) 
+
+elif choice == '3': print(num1,"*",num2,"=", multiply(num1,num2)) 
+
+elif choice == '4': print(num1,"/",num2,"=", divide(num1,num2)) 
+
+else: print("Input salah")
